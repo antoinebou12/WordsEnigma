3d38f137d7289f50b97159b0347e9fd509e17c9a
Antoine Boucher <antoine.bou13@gmail.com>
2022-06-14 12:14:20 -0400

    add logic

--- WordsEnigma/web/src/pages/MotduJourPage/subcomponents/Keyboard/Keyboard.tsx
+++ WordsEnigma/web/src/pages/MotduJourPage/subcomponents/Keyboard/Keyboard.tsx
@@ -6,7 +6,6 @@ interface IProps {
 }
 
 interface IState {
-
 }
 
 class Keyboard extends Component<IProps, IState> {
@@ -16,7 +15,11 @@ class Keyboard extends Component<IProps, IState> {
   }
 
   onClick(e) {
-    console.log(e.target.id);
+    console.log(e.target.innerText);
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
   }
 
   componentDidMount() {
@@ -31,38 +34,38 @@ class Keyboard extends Component<IProps, IState> {
     return (
       <div className="keyboard">
         <div className="line">
-          <div className="letter" id="letterQ">Q</div>
-          <div className="letter" id="letterW">W</div>
-          <div className="letter" id="letterE">E</div>
-          <div className="letter" id="letterR">R</div>
-          <div className="letter" id="letterT">T</div>
-          <div className="letter" id="letterY">Y</div>
-          <div className="letter" id="letterU">U</div>
-          <div className="letter" id="letterI">I</div>
-          <div className="letter" id="letterO">O</div>
-          <div className="letter" id="letterP">P</div>
+          <div className="letter" onClick={this.onClick}>Q</div>
+          <div className="letter" onClick={this.onClick}>W</div>
+          <div className="letter" onClick={this.onClick}>E</div>
+          <div className="letter" onClick={this.onClick}>R</div>
+          <div className="letter" onClick={this.onClick}>T</div>
+          <div className="letter" onClick={this.onClick}>Y</div>
+          <div className="letter" onClick={this.onClick}>U</div>
+          <div className="letter" onClick={this.onClick}>I</div>
+          <div className="letter" onClick={this.onClick}>O</div>
+          <div className="letter" onClick={this.onClick}>P</div>
         </div>
         <div className="line">
-          <div className="letter" id="letterA">A</div>
-          <div className="letter" id="letterS">S</div>
-          <div className="letter" id="letterD">D</div>
-          <div className="letter" id="letterF">F</div>
-          <div className="letter" id="letterG">G</div>
-          <div className="letter" id="letterH">H</div>
-          <div className="letter" id="letterJ">J</div>
-          <div className="letter" id="letterK">K</div>
-          <div className="letter" id="letterL">L</div>
+          <div className="letter" onClick={this.onClick}>A</div>
+          <div className="letter" onClick={this.onClick}>S</div>
+          <div className="letter" onClick={this.onClick}>D</div>
+          <div className="letter" onClick={this.onClick}>F</div>
+          <div className="letter" onClick={this.onClick}>G</div>
+          <div className="letter" onClick={this.onClick}>H</div>
+          <div className="letter" onClick={this.onClick}>J</div>
+          <div className="letter" onClick={this.onClick}>K</div>
+          <div className="letter" onClick={this.onClick}>L</div>
         </div>
         <div className="line">
-          <div className="deviner doubleSpace">Jouer</div>
-          <div className="letter" id="letterZ">Z</div>
-          <div className="letter" id="letterX">X</div>
-          <div className="letter" id="letterC">C</div>
-          <div className="letter" id="letterV">V</div>
-          <div className="letter" id="letterB">B</div>
-          <div className="letter" id="letterN">N</div>
-          <div className="letter" id="letterM">M</div>
-          <div className="suprm doubleSpace">Del</div>
+          <div className="doubleSpace" onClick={this.onClick}>Play</div>
+          <div className="letter" onClick={this.onClick}>Z</div>
+          <div className="letter" onClick={this.onClick}>X</div>
+          <div className="letter" onClick={this.onClick}>C</div>
+          <div className="letter" onClick={this.onClick}>V</div>
+          <div className="letter" onClick={this.onClick}>B</div>
+          <div className="letter" onClick={this.onClick}>N</div>
+          <div className="letter" onClick={this.onClick}>M</div>
+          <div className="doubleSpace" onClick={this.onClick}>Del</div>
         </div>
       </div>
     );
